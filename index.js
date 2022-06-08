0725b8dda60880ed55590b17648f64cb78ecf9dc
Douglas Obara <douglasobara97@gmail.com>
2022-06-08 16:44:25 +0300

    commit tree 7199872772952007d06ba26aeba7d808161732f1

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+// multiply is an equation whose multiplied results will equal 6
+const num1 = 62;
+const num2 = 1;
+
+const multiply = num1 * num2;
+
+//  random generates a random integer greater than 0
+const random = Math.ceil(Math.random());
+
+// mod is an equation that calculates a remainder that is equal to 4
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+// max will return 20 as the highest number in the set
+const max = Math.max(18, 20, 12, 4, 16, 7.8);
